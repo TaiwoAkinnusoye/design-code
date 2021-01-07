3c21febcdc9fc22af09577fa1e5dd0c8fdc795d0
taiwo <taiwo@kudabank.com>
2021-01-07 22:36:51 +0100

    intial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Design Code by Taiwo`,
+    description: `Re learn CSS and React; learn cool css stuff and modern-day JavaScript`,
     author: `@gatsbyjs`,
   },
   plugins: [
